a850618ccaaafaa0dbc157b2a4a22e29a1df5855
jarryhua <jarryhua@qq.com>
2018-10-19 23:22:39 +0800

    npm journey

---
+++ 0npm/npm-lessons.js
@@ -0,0 +1,8 @@
+console.log(`
+LESSON-001
+* upload the project configure need a pair files
+  1) pachage.json
+  2) package-lock.json
+  like their file names show, lock means lock specified version, otherwise
+  use the current or upper compatible version.
+`);
